e82b8d9be85fa37ea8e50729f67d2ea7dd3431bb
felix <ruiz.felixruiz@gmail.com>
2022-07-21 11:20:29 +0200

    Download attachments documents centrale dienst van eredienst

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ import {
     TASK_SUCCESS_STATUS, updateTaskStatus
 } from './lib/submission-task';
 
+import { isCentraalBestuurVanEredienstDocument } from './lib/utils';
+
 import { getAuthenticationConfigForSubmission, cleanCredentials } from './lib/credential-helpers';
 
 app.use(bodyParser.json({ type: function(req) { return /^application\/json/.test(req.get('content-type')); } }));
@@ -89,23 +91,43 @@ async function importSubmission(task, submission, documentUrl, submittedDocument
 }
 
 function calculateAttachmentsToDownlad(triples, submittedDocument){
-  // The most basic case where bestuurseenheid doesn't have to publish linked
-  // I.e. the decision has just an URI and refers to a PDF for ALL the rest.
-  // Mainly meant for VGC case
-  const attachmentsAsSourceOfDecisions = triples
-        .filter(t => t.subject == submittedDocument
-                && t.predicate == 'http://purl.org/dc/terms/source')
-        .map(t => t.object);
+  let allAttachments = [];
 
-  //This should cover simple attachments AND attachments as part of decision
-  const simpleAttachments = triples
-        .filter(t => t.subject == submittedDocument
-                && t.predicate == 'http://data.europa.eu/eli/ontology#related_to')
-        .map(t => t.object);
+  if(isCentraalBestuurVanEredienstDocument(submittedDocument, triples)) {
+    const relatedDocuments = triples
+          .filter(t => t.subject == submittedDocument
+                  && t.predicate == 'http://purl.org/dc/terms/relation')
+          .map(t => t.object);
 
-  //Nested decisions are ignored for now. Not sure what to expect from ABB, and how it should be rendered.
+    for(const docUri of relatedDocuments) {
+      const attachments = triples
+            .filter(t => t.subject == docUri
+                    && t.predicate == 'http://purl.org/dc/terms/source')
+            .map(t => t.object);
 
-  return [ ...attachmentsAsSourceOfDecisions, ...simpleAttachments ];
+      allAttachments = [ ...allAttachments, ...attachments ];
+    }
+  }
+  else {
+    // The most basic case where bestuurseenheid doesn't have to publish linked
+    // I.e. the decision has just an URI and refers to a PDF for ALL the rest.
+    // Mainly meant for VGC case
+    const attachmentsAsSourceOfDecisions = triples
+          .filter(t => t.subject == submittedDocument
+                  && t.predicate == 'http://purl.org/dc/terms/source')
+          .map(t => t.object);
+
+    //This should cover simple attachments AND attachments as part of decision
+    const simpleAttachments = triples
+          .filter(t => t.subject == submittedDocument
+                  && t.predicate == 'http://data.europa.eu/eli/ontology#related_to')
+          .map(t => t.object);
+
+    //Nested decisions are ignored for now. Not sure what to expect from ABB, and how it should be rendered.
+
+    allAttachments = [ ...attachmentsAsSourceOfDecisions, ...simpleAttachments ];
+  }
+  return allAttachments;
 }
 
 /**
