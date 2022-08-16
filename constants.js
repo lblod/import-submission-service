7f742526cce9401bdf0461b8e84f7a1953ef91c4
benjay10 <benbehaeghe@gmail.com>
2022-08-16 17:30:13 +0200

    Use cogs:Jobs and integrate with Job controller
    We now start from a task created by the job-controller with an input
    data container. Some functions have been removed or rewritten because of
    this. We also write different states to the tasks status to make sure
    the job-controller can pick up on the progress.
    All constants have been moved to a separate file.

--- constants.js
+++ constants.js
@@ -1,27 +1,51 @@
-export const PREFIXES = `
-  PREFIX meb:   <http://rdf.myexperiment.org/ontologies/base/>
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-  PREFIX pav:   <http://purl.org/pav/>
-  PREFIX dct:   <http://purl.org/dc/terms/>
-  PREFIX melding:   <http://lblod.data.gift/vocabularies/automatische-melding/>
-  PREFIX lblodBesluit:  <http://lblod.data.gift/vocabularies/besluit/>
-  PREFIX adms:  <http://www.w3.org/ns/adms#>
-  PREFIX muAccount:   <http://mu.semte.ch/vocabularies/account/>
-  PREFIX eli:   <http://data.europa.eu/eli/ontology#>
-  PREFIX org:   <http://www.w3.org/ns/org#>
-  PREFIX elod:  <http://linkedeconomy.org/ontology#>
-  PREFIX nie:   <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
-  PREFIX prov:  <http://www.w3.org/ns/prov#>
-  PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
-  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX nfo:   <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
-  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-  PREFIX http: <http://www.w3.org/2011/http#>
-  PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
-  PREFIX dgftSec: <http://lblod.data.gift/vocabularies/security/>
-  PREFIX dgftOauth: <http://kanselarij.vo.data.gift/vocabularies/oauth-2.0-session/>
-  PREFIX wotSec: <https://www.w3.org/2019/wot/security#>
-`;
+const PREFIX_TABLE = {
+  meb:          'http://rdf.myexperiment.org/ontologies/base/',
+  xsd:          'http://www.w3.org/2001/XMLSchema#',
+  pav:          'http://purl.org/pav/',
+  dct:          'http://purl.org/dc/terms/',
+  melding:      'http://lblod.data.gift/vocabularies/automatische-melding/',
+  lblodBesluit: 'http://lblod.data.gift/vocabularies/besluit/',
+  adms:         'http://www.w3.org/ns/adms#',
+  muAccount:    'http://mu.semte.ch/vocabularies/account/',
+  eli:          'http://data.europa.eu/eli/ontology#',
+  org:          'http://www.w3.org/ns/org#',
+  elod:         'http://linkedeconomy.org/ontology#',
+  nie:          'http://www.semanticdesktop.org/ontologies/2007/01/19/nie#',
+  prov:         'http://www.w3.org/ns/prov#',
+  mu:           'http://mu.semte.ch/vocabularies/core/',
+  foaf:         'http://xmlns.com/foaf/0.1/',
+  nfo:          'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#',
+  ext:          'http://mu.semte.ch/vocabularies/ext/',
+  http:         'http://www.w3.org/2011/http#',
+  rpioHttp:     'http://redpencil.data.gift/vocabularies/http/',
+  dgftSec:      'http://lblod.data.gift/vocabularies/security/',
+  dgftOauth:    'http://kanselarij.vo.data.gift/vocabularies/oauth-2.0-session/',
+  wotSec:       'https://www.w3.org/2019/wot/security#',
+  task:         'http://redpencil.data.gift/vocabularies/tasks/',
+};
+
+export const PREFIXES = (() => {
+  const all = [];
+  for (const key in PREFIX_TABLE)
+    all.push(`PREFIX ${key}: <${PREFIX_TABLE[key]}>`);
+  return all.join('\n');
+})();
+
+export function getPrefixes(collOfPrefixes) {
+  const all = [];
+  for (const key of collOfPrefixes)
+    all.push(`PREFIX ${key}: <${PREFIX_TABLE[key]}>`);
+  return all.join('\n');
+}
 
 export const BASIC_AUTH = 'https://www.w3.org/2019/wot/security#BasicSecurityScheme';
 export const OAUTH2 = 'https://www.w3.org/2019/wot/security#OAuth2SecurityScheme';
+
+export const TASK_ONGOING_STATUS = 'http://redpencil.data.gift/id/concept/JobStatus/busy';
+export const TASK_SUCCESS_STATUS = 'http://redpencil.data.gift/id/concept/JobStatus/success';
+export const TASK_FAILURE_STATUS = 'http://redpencil.data.gift/id/concept/JobStatus/failed';
+
+export const OPERATION_PREDICATE = 'http://redpencil.data.gift/vocabularies/tasks/operation';
+export const IMPORT_OPERATION = 'http://lblod.data.gift/id/jobs/concept/TaskOperation/import';
+
+export const CREATOR = 'http://lblod.data.gift/services/import-submission-service';
