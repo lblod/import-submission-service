d68545c963409f701e2fd0d71ef289f1a7c9b42c
benjay10 <ben.behaeghe@redpencil.io>
2022-09-20 05:38:50 +0200

    Automatic Linting fixes and query rewrites

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ import { app, errorHandler } from 'mu';
 import { scheduleDownloadAttachment } from './lib/attachment-helpers';
 import { loadFileData, writeTtlFile } from './lib/file-helpers';
 import RdfaExtractor from './lib/rdfa-extractor';
-import { enrichSubmission, enrichWithAttachmentInfo } from './lib/submission-enricher';
-import { getRemoteDataObjectUris, getSubmissionInfo } from './lib/submission-task';
+import {
+  enrichSubmission,
+  enrichWithAttachmentInfo,
+} from './lib/submission-enricher';
+import {
+  getRemoteDataObjectUris,
+  getSubmissionInfo,
+} from './lib/submission-task';
 import * as env from './constants.js';
 import { saveError, isCentraalBestuurVanEredienstDocument } from './lib/utils';
-import { getAuthenticationConfigForSubmission, cleanCredentials } from './lib/credential-helpers';
 import { updateTaskStatus } from './lib/submission-task.js';
 
-app.use(bodyParser.json({ type: function(req) { return /^application\/json/.test(req.get('content-type')); } }));
+app.use(
+  bodyParser.json({
+    type: function (req) {
+      return /^application\/json/.test(req.get('content-type'));
+    },
+  })
+);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Hello from import-submission-service');
 });
 
-app.post('/delta', async function(req, res, next) {
+app.post('/delta', async function (req, res) {
   //We can already send a 200 back. The delta-notifier does not care about the result, as long as the request is closed.
   res.status(200).send().end();
 
@@ -39,90 +50,123 @@ app.post('/delta', async function(req, res, next) {
           const importedFileUri = await importSubmission(remoteDataObject);
           importedFileUris.push(importedFileUri);
         }
-        await updateTaskStatus(taskUri, env.TASK_SUCCESS_STATUS, undefined, importedFileUris);
-      }
-      catch (error) {
+        await updateTaskStatus(
+          taskUri,
+          env.TASK_SUCCESS_STATUS,
+          undefined,
+          importedFileUris
+        );
+      } catch (error) {
         const message = `Something went wrong while importing for task ${taskUri}`;
         console.error(`${message}\n`, error.message);
         console.error(error);
-        const errorUri = await saveError({ message, detail: error.message, });
+        const errorUri = await saveError({ message, detail: error.message });
         await updateTaskStatus(taskUri, env.TASK_FAILURE_STATUS, errorUri);
       }
     }
-  }
-  catch (error) {
-    const message = 'The task for importing a submission could not even be started or finished due to an unexpected problem.';
+  } catch (error) {
+    const message =
+      'The task for importing a submission could not even be started or finished due to an unexpected problem.';
     console.error(`${message}\n`, error.message);
     console.error(error);
-    await saveError({ message, detail: error.message, });
+    await saveError({ message, detail: error.message });
   }
 });
 
 async function importSubmission(remoteDataObject) {
-  const { submission, documentUrl, submittedDocument, fileUri } = await getSubmissionInfo(remoteDataObject);
+  const { submission, documentUrl, submittedDocument, fileUri } =
+    await getSubmissionInfo(remoteDataObject);
   const html = await loadFileData(fileUri);
   const rdfaExtractor = new RdfaExtractor(html, documentUrl);
   const triples = rdfaExtractor.rdfa();
-  const enrichments = await enrichSubmission(submittedDocument, fileUri, remoteDataObject, triples, documentUrl);
+  const enrichments = await enrichSubmission(
+    submittedDocument,
+    fileUri,
+    remoteDataObject,
+    triples,
+    documentUrl
+  );
   rdfaExtractor.add(enrichments);
 
-  const attachmentUrls = calculateAttachmentsToDownlad(triples, submittedDocument);
+  const attachmentUrls = calculateAttachmentsToDownlad(
+    triples,
+    submittedDocument
+  );
 
-  if(attachmentUrls.length){
+  if (attachmentUrls.length) {
     console.log(`Found attachments: ${attachmentUrls.join('\n')}`);
-    for(const attachmentUrl of attachmentUrls){
+    for (const attachmentUrl of attachmentUrls) {
       //Note: there is no clear message when attachment download failed.
-      const remoteDataObject = await scheduleDownloadAttachment(submission, attachmentUrl);
-      const enrichments = await enrichWithAttachmentInfo(submittedDocument, remoteDataObject, attachmentUrl);
+      const remoteDataObject = await scheduleDownloadAttachment(
+        submission,
+        attachmentUrl
+      );
+      const enrichments = await enrichWithAttachmentInfo(
+        submittedDocument,
+        remoteDataObject,
+        attachmentUrl
+      );
       rdfaExtractor.add(enrichments);
     }
   }
 
   const ttl = rdfaExtractor.ttl();
   const uri = await writeTtlFile(ttl, submittedDocument, remoteDataObject);
-  console.log(`Successfully extracted data for submission <${submission}> from remote file <${remoteDataObject}> to <${uri}>`);
+  console.log(
+    `Successfully extracted data for submission <${submission}> from remote file <${remoteDataObject}> to <${uri}>`
+  );
   return uri;
 }
 
-function calculateAttachmentsToDownlad(triples, submittedDocument){
+function calculateAttachmentsToDownlad(triples, submittedDocument) {
   let allAttachments = [];
 
-  if(isCentraalBestuurVanEredienstDocument(submittedDocument, triples)) {
+  if (isCentraalBestuurVanEredienstDocument(submittedDocument, triples)) {
     const relatedDocuments = triples
-          .filter(t => t.subject == submittedDocument
-                  && t.predicate == 'http://purl.org/dc/terms/relation')
-          .map(t => t.object);
+      .filter(
+        (t) =>
+          t.subject == submittedDocument &&
+          t.predicate == 'http://purl.org/dc/terms/relation'
+      )
+      .map((t) => t.object);
 
-    for(const docUri of relatedDocuments) {
+    for (const docUri of relatedDocuments) {
       const attachments = triples
-            .filter(t => t.subject == docUri
-                    && t.predicate == 'http://purl.org/dc/terms/source')
-            .map(t => t.object);
+        .filter(
+          (t) =>
+            t.subject == docUri &&
+            t.predicate == 'http://purl.org/dc/terms/source'
+        )
+        .map((t) => t.object);
 
-      allAttachments = [ ...allAttachments, ...attachments ];
+      allAttachments = [...allAttachments, ...attachments];
     }
-  }
-  else {
+  } else {
     // The most basic case where bestuurseenheid doesn't have to publish linked
     // I.e. the decision has just an URI and refers to a PDF for ALL the rest.
     // Mainly meant for VGC case
     const attachmentsAsSourceOfDecisions = triples
-          .filter(t => t.subject == submittedDocument
-                  && t.predicate == 'http://purl.org/dc/terms/source')
-          .map(t => t.object);
+      .filter(
+        (t) =>
+          t.subject == submittedDocument &&
+          t.predicate == 'http://purl.org/dc/terms/source'
+      )
+      .map((t) => t.object);
 
     //This should cover simple attachments AND attachments as part of decision
     const simpleAttachments = triples
-          .filter(t => t.subject == submittedDocument
-                  && t.predicate == 'http://data.europa.eu/eli/ontology#related_to')
-          .map(t => t.object);
+      .filter(
+        (t) =>
+          t.subject == submittedDocument &&
+          t.predicate == 'http://data.europa.eu/eli/ontology#related_to'
+      )
+      .map((t) => t.object);
 
     //Nested decisions are ignored for now. Not sure what to expect from ABB, and how it should be rendered.
 
-    allAttachments = [ ...attachmentsAsSourceOfDecisions, ...simpleAttachments ];
+    allAttachments = [...attachmentsAsSourceOfDecisions, ...simpleAttachments];
   }
   return allAttachments;
 }
 
 app.use(errorHandler);
-
