43dff10d004c36fe06a14693caee3e5d2d1d9d56
felix <ruiz.felixruiz@gmail.com>
2023-05-18 12:06:57 +0200

    Centrale-besturen can also have normal attachments

--- app.js
+++ app.js
@@ -109,27 +109,25 @@ function calculateAttachmentsToDownlad(triples, submittedDocument){
       allAttachments = [ ...allAttachments, ...attachments ];
     }
   }
-  else {
-    // The most basic case where bestuurseenheid doesn't have to publish linked
-    // I.e. the decision has just an URI and refers to a PDF for ALL the rest.
-    // Mainly meant for VGC case
-    const attachmentsAsSourceOfDecisions = triples
-          .filter(t => t.subject == submittedDocument
-                  && t.predicate == 'http://purl.org/dc/terms/source')
-          .map(t => t.object);
 
-    //This should cover simple attachments AND attachments as part of decision
-    const simpleAttachments = triples
-          .filter(t => t.subject == submittedDocument
-                  && t.predicate == 'http://data.europa.eu/eli/ontology#related_to')
-          .map(t => t.object);
+  // The most basic case where bestuurseenheid doesn't have to publish linked
+  // I.e. the decision has just an URI and refers to a PDF for ALL the rest.
+  // Mainly meant for VGC case
+  const attachmentsAsSourceOfDecisions = triples
+        .filter(t => t.subject == submittedDocument
+                && t.predicate == 'http://purl.org/dc/terms/source')
+        .map(t => t.object);
 
-    //Nested decisions are ignored for now. Not sure what to expect from ABB, and how it should be rendered.
+  //This should cover simple attachments AND attachments as part of decision
+  const simpleAttachments = triples
+        .filter(t => t.subject == submittedDocument
+                && t.predicate == 'http://data.europa.eu/eli/ontology#related_to')
+        .map(t => t.object);
 
-    allAttachments = [ ...attachmentsAsSourceOfDecisions, ...simpleAttachments ];
-  }
+  //Nested decisions are ignored for now. Not sure what to expect from ABB, and how it should be rendered.
+  allAttachments = [ ...allAttachments, ...attachmentsAsSourceOfDecisions, ...simpleAttachments ];
+
   return allAttachments;
 }
 
 app.use(errorHandler);
-
